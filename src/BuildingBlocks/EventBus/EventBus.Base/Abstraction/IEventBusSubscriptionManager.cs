df7405788cf6aa5b7082c88e1ac9791a61afb9cf
Grany35 <ogun.ergin35@gmail.com>
2023-02-27 21:49:39 +0300

    Added InMemoryEventBusSubscriptionManager

--- src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBusSubscriptionManager.cs
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBusSubscriptionManager.cs
@@ -4,27 +4,25 @@ namespace EventBus.Base.Abstraction;
 
 public interface IEventBusSubscriptionManager
 {
-    bool IsEmpty { get; }
-
+    public bool IsEmpty { get; }
+    
     event EventHandler<string> OnEventRemoved;
-
+    
     void AddSubscription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
-
+    
     void RemoveSubscription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
-
-    bool HasSubscriptionsForEvent<T>() where T : IntegrationEvent;
-
-    bool HasSubscriptionsForEvent<T>(string eventName) where T : IntegrationEvent;
-
-    bool HasSubscriptionsForEvent(string eventName);
-
+    
+    bool HasSubscriptionForEvent<T>() where T : IntegrationEvent;
+    
+    bool HasSubscriptionForEvent(string eventName);
+    
     Type GetEventTypeByName(string eventName);
-
+    
     void Clear();
-
+    
     IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent;
-
+    
     IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName);
-
-    string GetEventKey<T>();
+    
+    string GetEventKey<T>(); // routing key 
 }
